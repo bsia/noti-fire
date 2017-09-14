f6b99608660cddc962edef3c3ed4693fd888a499
Barry Sia <bsia@mocana.com>
2017-09-14 12:11:43 -0700

    Add bunch of commands

--- index.js
+++ index.js
@@ -2,15 +2,39 @@
 
 var program = require('commander');
 
-// program.arguments('<file>')
 
+program.usage('<cmd>');
 
-program
-  .usage('<cmd> [options]')
-  .command('topic [options]', 'Send a topic notification')
-  .command('token [options]', 'Send to an fcm token')
+program.version('0.0.1');
+
+program.command('topic')
+  .option('-t, --topic', 'Notification topic')
+  .description('Send notification by topic')
+  .action(function() {
+    console.log('topic command');
+  });
+  // .command('token [options]', 'Send to an fcm token')
   // .action(function(cmd) {
   //   console.log('fcm_token: %s, cmd: %s',
   //       program.fcm_token, cmd);
   // })
-  .parse(process.argv);
+
+program.command('single')
+  .option('-f, --fcm_token', 'Destination FCM token ')
+  .description('Send notification to a single device')
+  .action(function() {
+      console.log('single command');
+  });
+
+program.command('*')
+  .description('no command')
+  .action(function() {
+    console.log('command must be specified.');
+  });
+
+program.parse(process.argv);
+
+// Set default command to help
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
